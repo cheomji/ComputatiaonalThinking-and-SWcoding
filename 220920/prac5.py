8e414a7f0865d9ed3298b7f1112174a036e08218
cheomji <104716784+cheomji@users.noreply.github.com>
2022-09-21 03:10:17 +0900

    Create prac5.py
    파이썬 제어문 배운날(for문& if문)

---
+++ 220920/prac5.py
@@ -0,0 +1,19 @@
+s = int(input('start > '))
+e = int(input('end > '))
+n = int(input('sum > '))
+cnt = 0
+for i in range(10000, 1000000):
+    num = [i//10000, i%10000//1000, i%10000%1000//100, i%10000%1000%100//10, i%10]
+    check = True
+    for j in range(0, 5):
+        if num[j] not in range(s, e+1):
+            check = False
+            break
+    if check == True:
+        if num[0]+num[1]+num[2]+num[3]+num[4] == n:
+            print(num[0], num[1], num[2], num[3], num[4], sep='')
+            cnt += 1
+            if cnt != 0 and cnt % 5 == 0:
+                print()
+if cnt == 0:
+    print('No answer')
