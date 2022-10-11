31bdda0ec993ab2d80d97d9f82e0fd1feda4f2d8
cheomji <104716784+cheomji@users.noreply.github.com>
2022-10-11 16:35:01 +0900

    Update Lab4_practice#1.py

--- 221011/Lab4_practice#1.py
+++ 221011/Lab4_practice#1.py
@@ -2,6 +2,9 @@ def binaryToDecimal(numBinary):
     numDecimal = 0
     for i in range(0, len(numBinary)):
         numDecimal += numBinary[i] * (2 ** (len(numBinary)-1-i))
+    #교수님 코드
+    #lst = [i**2 for i in range(len(numBinary)-1, -1, -1)] -> 16 8 4 2 1 이런 리스트 만들어주는거
+    #for d in lst: 이렇게 해서 ... 어 .. numBinary 요소들 돌면서 저거 곱해주면됨ㅇㅇ
     return numDecimal
 
 nums = [1,0,0,1]
